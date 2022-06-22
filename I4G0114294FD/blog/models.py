567c486e957bc99abd577248b9078b9341204cfa
Librapraise <102430199+Librapraise@users.noreply.github.com>
2022-06-22 20:33:10 +0100

    Update models.py

--- I4G0114294FD/blog/models.py
+++ I4G0114294FD/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
+from django.utils import timezone
 
 User = get_user_model()
 # Create your models here.
